f18da3520226b0e5dcb5b5fdaa99e3810e0279c8
Xaver Buttinger <xaver@emmgt.at>
2021-05-08 11:04:20 +0200

    init mpparser

--- 7_ATG/MPParser.pas
+++ 7_ATG/MPParser.pas
@@ -24,37 +24,158 @@ PROCEDURE Parse(var inputFile: TEXT; VAR ok: BOOLEAN);
 BEGIN
     InitScanner(inputFile);
     success := TRUE;
-    // TODO MP
+    MP;
     ok := success;
 
 END;
 
 PROCEDURE MP;
 BEGIN
+    IF currentSymbol <> programSym THEN BEGIN success := FALSE; Exit; End;
+    GetNextSymbol;
+   
+   IF currentSymbol <> identSym THEN BEGIN success := FALSE; Exit; End;
+    GetNextSymbol;
+
+    IF currentSymbol <> semicolonSym THEN BEGIN success := FALSE; Exit; End;
+    GetNextSymbol;
+   
+    IF currentSymbol = varSym THEN BEGIN; // optional nonterminal
+    VarDecl; IF NOT success THEN Exit;
+    END;
+
+    IF currentSymbol <> beginSym THEN BEGIN success := FALSE; Exit; End;
+    GetNextSymbol;
+
+    StatSeq; IF NOT success THEN Exit;
+
+    IF currentSymbol <> endSym THEN BEGIN success := FALSE; Exit; End;
+    GetNextSymbol;
+
+    IF currentSymbol <> periodSym THEN BEGIN success := FALSE; Exit; End;
+    GetNextSymbol;
 END;
 
 PROCEDURE VarDecl;
 BEGIN
+
+    IF currentSymbol <> varSym THEN BEGIN success := FALSE; Exit; End;
+    GetNextSymbol;
+
+    IF currentSymbol <> identSym THEN BEGIN success := FALSE; Exit; End;
+    GetNextSymbol;
+    WHILE currentSymbol = commaSym DO BEGIN // 1 oder mehrmals
+       GetNextSymbol;
+       IF currentSymbol <> identSym THEN BEGIN success := FALSE; Exit; End;
+       GetNextSymbol;
+    END;
+
+   IF currentSymbol <> colonSym THEN BEGIN success := FALSE; Exit; End;
+    GetNextSymbol;
+
+   IF currentSymbol <> integerSym THEN BEGIN success := FALSE; Exit; End;
+   GetNextSymbol;
+
+   IF currentSymbol <> semicolonSym THEN BEGIN success := FALSE; Exit; End;
+   GetNextSymbol;
+
 END;
 
 PROCEDURE StatSeq;
 BEGIN
+  Stat; IF NOT success THEN Exit;
+  WHILE currentSymbol = semicolonSym DO BEGIN
+     GetNextSymbol;
+     Stat; IF NOT success THEN Exit;
+  END;
 END;
 
 PROCEDURE Stat;
 BEGIN
-END;
+    CASE currentSymbol OF 
+         identSym: BEGIN
+              GetNextSymbol;
+              IF currentSymbol <> assignSym THEN BEGIN success := FALSE; Exit; End;
+              GetNextSymbol;
+              Expr; IF NOT success THEN Exit;
 
-PROCEDURE Expr;
-BEGIN
-END;
+         END;
+         readSym: BEGIN
+            GetNextSymbol;
+            IF currentSymbol <> leftParSym THEN BEGIN success := FALSE; Exit; End;
+            GetNextSymbol;
 
-PROCEDURE Term;
-BEGIN
+            IF currentSymbol <> identSym THEN BEGIN success := FALSE; Exit; End;
+              GetNextSymbol;
+
+              IF currentSymbol <> rightParSym THEN BEGIN success := FALSE; Exit; End;
+              GetNextSymbol;
+            
+         END;
+         writeSym: BEGIN
+           GetNextSymbol;
+            IF currentSymbol <> leftParSym THEN BEGIN success := FALSE; Exit; End;
+            GetNextSymbol;
+
+            Expr; IF NOT success THEN Exit;
+
+            IF currentSymbol <> rightParSym THEN BEGIN success := FALSE; Exit; End;
+            GetNextSymbol;
+         END;
+    END;
 END;
 
 PROCEDURE Fact;
 BEGIN
+    CASE CurrentSymbol OF
+        identSym: BEGIN
+            GetNextSymbol;
+        END;
+        numberSym: BEGIN
+            GetNextSymbol;
+        END;
+        leftParSym: BEGIN
+            GetNextSymbol;
+            Expr; IF NOT success THEN Exit;
+            IF CurrentSymbol <> rightParSym THEN BEGIN success := FALSE; Exit; END;
+            GetNextSymbol;
+        END;
+        ELSE BEGIN success := FALSE; Exit; END;
+    END;
+END;
+
+PROCEDURE Term;
+BEGIN
+ Fact; IF NOT success THEN Exit;
+    WHILE (CurrentSymbol = multSym) OR (CurrentSymbol = divSym) DO BEGIN
+        CASE CurrentSymbol OF
+            multSym: BEGIN
+                GetNextSymbol;
+                Fact; IF NOT success THEN Exit;
+            END;
+            divSym: BEGIN
+                GetNextSymbol;
+                Fact; IF NOT success THEN Exit;
+            END;
+        END;
+    END;
+END;
+
+PROCEDURE Expr;
+BEGIN
+    Term; IF NOT success THEN Exit;
+    WHILE (CurrentSymbol = plusSym) OR (CurrentSymbol = minusSym) DO BEGIN
+        CASE CurrentSymbol OF
+            plusSym: BEGIN
+                GetNextSymbol;
+                Term; IF NOT success THEN Exit;
+            END;
+            minusSym: BEGIN
+                GetNextSymbol;
+                Term; IF NOT success THEN Exit;
+            END;
+        END;
+    END;
 END;
 
 BEGIN
--- 7_ATG/MPScanner.pas
+++ 7_ATG/MPScanner.pas
@@ -19,26 +19,123 @@ FUNCTION CurrentNumberValue: INTEGER;
 FUNCTION CurrentIdentName: STRING;
 
 IMPLEMENTATION
+CONST
+	Tab = Chr(9);
+	LF = Chr(10);
+	CR = Chr(13);
+	Blank = ' ';
 
-PROCEDURE InitScanner(var inputFile: TEXT);
+VAR
+	inpFile: TEXT;
+	curChar: CHAR;
+	charLine, charColumn: INTEGER;
+
+	curSymbol: Symbol;
+	symbolLine, symbolColumn: INTEGER;
+
+	curNumberValue: INTEGER;
+	curIdentName: STRING;
+
+PROCEDURE GetNextChar;
 BEGIN
+	Read(inpFile, curChar);
+	(* handle new line *)
+	IF (curChar = CR) OR (curChar = LF) THEN BEGIN
+		(* REMARK: line count like this is not correct when file has CR line separators (which should not be a problem nowadays) *)
+		IF curChar = LF THEN
+			Inc(charLine);
+		charColumn := 0;
+	END ELSE
+		Inc(charColumn);
+END;
+
+PROCEDURE InitScanner(VAR inputFile: TEXT);
+BEGIN
+	inpFile := inputFile;
+	charLine := 1;
+	charColumn := 0;
+	GetNextChar;
+	GetNextSymbol;
 END;
 
 PROCEDURE GetNextSymbol;
+VAR
+	temp: STRING;
 BEGIN
+	(* skip whitespace *)
+	WHILE (curChar = Blank) OR (curChar = Tab) OR (curChar = CR) OR (curChar = LF) DO
+		GetNextChar;
+	(* parse next symbol *)
+	symbolLine := charLine;
+	symbolColumn := charColumn;
+	CASE curChar OF
+		'+': BEGIN curSymbol := plusSym; GetNextChar; END;
+		'-': BEGIN curSymbol := minusSym; GetNextChar; END;
+		'*': BEGIN curSymbol := multSym; GetNextChar; END;
+		'/': BEGIN curSymbol := divSym; GetNextChar; END;
+		'(': BEGIN curSymbol := leftParSym; GetNextChar; END;
+		')': BEGIN curSymbol := rightParSym; GetNextChar; END;
+		'.': BEGIN curSymbol := periodSym; GetNextChar; END;
+		',': BEGIN curSymbol := commaSym; GetNextChar; END;
+		';': BEGIN curSymbol := semicolonSym; GetNextChar; END;
+		':': BEGIN
+			GetNextChar;
+			IF curChar = '=' THEN BEGIN
+				curSymbol := assignSym;
+				GetNextChar;
+			END ELSE
+				curSymbol := colonSym;
+		END;
+		'a'..'z', 'A'..'Z', '_': BEGIN
+			temp := '';
+			WHILE curChar IN ['a'..'z', 'A'..'Z', '0'..'9', '_'] DO BEGIN
+				temp := temp + UpCase(curChar);
+				GetNextChar;
+			END;
+			IF temp = 'BEGIN' THEN
+				curSymbol := beginSym
+			ELSE IF temp = 'END' THEN
+				curSymbol := endSym
+			ELSE IF temp = 'INTEGER' THEN
+				curSymbol := integerSym
+			ELSE IF temp = 'PROGRAM' THEN
+				curSymbol := programSym
+			ELSE IF temp = 'READ' THEN
+				curSymbol := readSym
+			ELSE IF temp = 'VAR' THEN
+				curSymbol := varSym
+			ELSE IF temp = 'WRITE' THEN
+				curSymbol := writeSym
+			ELSE BEGIN
+				curSymbol := identSym;
+				curIdentName := temp;
+			END;
+		END;
+		'0'..'9': BEGIN
+			curSymbol := numberSym;
+			curNumberValue := 0;
+			WHILE curChar IN ['0'..'9'] DO BEGIN
+				curNumberValue := (curNumberValue * 10) + (Ord(curChar) - Ord('0'));
+				GetNextChar;
+			END;
+		END;
+		ELSE BEGIN curSymbol := noSym; GetNextChar; END;
+	END;
 END;
 
 FUNCTION CurrentSymbol: Symbol;
 BEGIN
+	CurrentSymbol := curSymbol;
 END;
 
 FUNCTION CurrentNumberValue: INTEGER;
 BEGIN
+	CurrentNumberValue := curNumberValue;
 END;
 
 FUNCTION CurrentIdentName: STRING;
 BEGIN
+	CurrentIdentName := curIdentName;
 END;
-
 
 END.
